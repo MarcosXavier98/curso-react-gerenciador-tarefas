a1cdd843681ba5878e6d3519de1a1d040e95d29b
Marcos Xavier <mrcsdevbr@gmail.com>
2026-04-14 13:32:29 -0300

    funções e icons

--- curso-react/src/components/Tasks.jsx
+++ curso-react/src/components/Tasks.jsx
@@ -1,9 +1,29 @@
-function Tasks(){
-    return(
-        <div>
-            <h2>Tasks</h2>
-        </div>
-    )
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <div>
+      <ul className="space-y-4 p-6 bg-slate-200 shadow-md rounded-md">
+        {props.tasks.map((task) => (
+          <li key={task.id} className="flex gap-2">
+            <button
+              onClick={() => props.onTaskClick(task.id)}
+              className={`bg-slate-400 text-left w-full text-white p-2 rounded-md 
+                ${task.isCompleted && "line-through"}`}
+            >
+              {task.title}
+            </button>
+            <button className="bg-slate-400 text-white p-2 rounded-md">
+              <ChevronRightIcon />
+            </button>
+            <button className="bg-slate-400 text-white p-2 rounded-md">
+                <Trash2Icon />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Tasks
+export default Tasks;
